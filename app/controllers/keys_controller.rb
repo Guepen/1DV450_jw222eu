0fcd8117e0ae9090b66837736e779787b407ac90
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-01-27 20:28:30 +0100

    Utöka listning av nycklar

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -2,6 +2,8 @@ class KeysController < ApplicationController
     before_filter :require_login
 
     def index
+        user = session[:user]
 
+        @key = Key.where(user_id: user).take
     end
 end
