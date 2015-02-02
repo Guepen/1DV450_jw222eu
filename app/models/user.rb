dc58610348ccb1d8848aab40ff66f04107fdbb63
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-02-02 20:06:07 +0100

    Se till så att e-post är unik

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
 
+    validates_uniqueness_of :email, :case_sensitive => false
     validates :password, length: { minimum: 8 }, allow_nil: true
 
     attr_accessor :app
