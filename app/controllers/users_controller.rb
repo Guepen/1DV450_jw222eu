f047f914018b16c1c20de5b0ff687b3f9c87ca79
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-02-08 16:58:54 +0100

    Bättre validering vid uppdatring av inställningar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,12 +44,12 @@ class UsersController < ApplicationController
 
         user = User.find_by(email: params[:email])
         if !user
-            return redirect_to root_path, :notice => 'Fel användarnamn eller lösenord'
+            return redirect_to root_path, :notice => 'Fel användarnamn eller lösenord.'
         end
 
         user = user.authenticate(params[:password])
         if !user
-            return redirect_to root_path, :notice => 'Fel användarnamn eller lösenord'
+            return redirect_to root_path, :notice => 'Fel användarnamn eller lösenord.'
         end
 
         session[:user] = user.id
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
         require_login
         reset_session
 
-        redirect_to root_path, :notice => 'Du är utloggad'
+        redirect_to root_path, :notice => 'Du är utloggad.'
     end
 
     def register
@@ -88,7 +88,10 @@ class UsersController < ApplicationController
         is_valid = user.save
 
         if !is_valid
-            return redirect_to users_settings_path, :notice => 'Alla fält måste fyllas i.'
+            flash[:notice] = user.errors
+            flash[:fields] = params
+
+            return redirect_to users_settings_path
         end
 
         redirect_to users_settings_path
